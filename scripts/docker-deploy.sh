002e5d06635cb04bab3be1d0f83e4c688edc31c2
yasin <yasin@rnssol.com>
2022-08-05 16:39:10 +0500

    docker deploy script updated

---
+++ scripts/docker-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Start of Script\n"
+
+echo "Stopping Previous Container $1"
+
+/usr/bin/docker stop $1
+
+echo "Removing Previous Container $1"
+
+/usr/bin/docker rm -f $1
+
+echo "Removing Previous Image $2"
+
+/usr/bin/docker rmi $2
+
+echo "Fetching and Deploying New Image"
+
+/usr/bin/docker run -d -p 4000:3000 --name $1 $2
+
+echo "End of Script"
